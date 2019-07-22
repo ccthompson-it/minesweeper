2a9d1747291d714785f288198c173559786bddfa
Caleb Thompson <caleb.thompson@xtra.co.nz>
2019-07-23 11:40:46 +1200

    added game resets and random bombs

--- minesweeper.js
+++ minesweeper.js
@@ -1,22 +1,42 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
+var boardSize = 3
 var board = {
   cells: [
-    {row:0, col:0, isMine: true, isMarked: false, hidden: true}, 
-    {row:0, col:1, isMine: false, isMarked: false, hidden: true},
-    {row:0, col:2, isMine: false, isMarked: false, hidden: true},
-    {row:1, col:0, isMine: false, isMarked: false, hidden: true}, 
-    {row:1, col:1, isMine: false, isMarked: false, hidden: true}, 
-    {row:1, col:2, isMine: true, isMarked: false, hidden: true},
-    {row:2, col:0, isMine: true, isMarked: false, hidden: true}, 
-    {row:2, col:1, isMine: false, isMarked: false, hidden: true}, 
-    {row:2, col:2, isMine: false, isMarked: false, hidden: true}
   ]
+}
 
+
+function playAgain(){
+  if (confirm("No, I think you should play again!")) {
+    startGame()
+  } else {
+    playAgain()
+  }
+}
+function createBoard(size){
+  board.cells = []
+  for( var i=0; i<size; i++){
+    for( var j=0; j<size; j++){
+      var cell = {
+        row: i,
+        col: j,
+        isMine: false,
+        isMarked: false,
+        hidden: true,
+      }
+      var random = Math.floor(Math.random() * 10)
+      if( random > 6.5){
+        cell.isMine = true
+      }
+      board.cells.push(cell)
+    }
+  }
 }
 
 function startGame () {
+  createBoard(boardSize)
   for( var i=0; i<board.cells.length; i++){
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
@@ -24,6 +44,7 @@ function startGame () {
   document.addEventListener("contextmenu", checkForWin)
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
+  
 }
 
 // Define this function to look for a win condition:
@@ -39,8 +60,12 @@ function checkForWin () {
       return
     }
   }
-  lib.displayMessage('You win!')
-  alert("You Win!")
+  
+  if (confirm("Congratulations! Play Again?")) {
+    startGame()
+  } else {
+    playAgain()
+  }
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
